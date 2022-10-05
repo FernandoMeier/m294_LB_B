0949bc7b4354192242a576a9f30712b9e75e6893
Ferni <meierf@bzz.ch>
2022-10-05 21:27:06 +0200

    feat: dir for all main functions

---
+++ GET_Task/script.js
@@ -0,0 +1,35 @@
+const createCell = (cellText) => {
+    const cell = document.createElement("td");
+    cell.innerText = cellText;
+    return cell
+}
+
+const addTask = (task) => {
+    const display = document.getElementById("display");
+    const tableRow = document.createElement("tr");
+
+    for(let inde = task.length; inde >= 0; inde--) {
+        inde--;
+        tableRow.append(
+            createCell(task[inde].id),
+            createCell(task[inde].title),
+            createCell(task[inde].completed)
+        )
+    }
+
+    
+
+    display.appendChild(tableRow);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch("http://localhost:3000/tasks")
+    .then((response)=> {
+        return response.json();
+    })
+    .then((data) =>{
+        addTask(data)
+    })
+
+});
